ca41874a3afb1be9dd503bb6c3771d968cbac3bf
Stefan <stefan_aleksik@live.com>
2020-12-30 11:48:35 +0100

    update pie

--- src/App.js
+++ src/App.js
@@ -4,19 +4,7 @@ import Pie from './components/Pie/Pie'
 
 class App extends React.Component {
   state = {
-    pieData: [
-      { label: "Bozo", value: 1 },
-      { label: "Laze", value: 1 },
-      { label: "Tomi", value: 1 },
-      { label: "Kate", value: 1 },
-      { label: "Maca", value: 1 },
-      { label: "Stef", value: 1 },
-      { label: "Marche", value: 1 },
-      { label: "Ante", value: 1 },
-      { label: "Viski", value: 1 },
-      { label: "Buli", value: 1 },
-      { label: "Aleksikj", value: 1 },
-    ]
+    pieData: [    ]
   }
 
   render() {
